ee55e27924da9fe657a947a78d50f59330f589df
Md Kasif Uddin <mdkasifuddin121@gmail.com>
2025-09-11 07:26:41 +0000

    In the add new friend modal use this name @MdKasif

--- src/components/chat/new-chat-dialog.tsx
+++ src/components/chat/new-chat-dialog.tsx
@@ -142,7 +142,7 @@ export function NewChatDialog({ children, onChatSelect }: { children: React.Reac
                                             <AtSign className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
                                             <Input 
                                                 id="username" 
-                                                placeholder="jane_cooper" 
+                                                placeholder="MdKasif" 
                                                 className="pl-10 h-12 text-base" 
                                                 {...field} 
                                             />
